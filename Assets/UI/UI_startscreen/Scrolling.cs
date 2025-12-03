75efc6c29caf56c5d5a3e8cdfec2203c29c2ca1b
Timtimwafflehouse <timmylemmer2000@gmail.com>
2025-12-03 15:15:22 +0100

    added the UI for startscreen and the swipe cards
    added the UI for startscreen and the swipe cards. Unity crashed so probably every changes I made in my own start screen scene might have been lost

---
+++ Assets/UI/UI_startscreen/Scrolling.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scrolling : MonoBehaviour
+{
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private float _x, _y;
+
+    private void Update()
+    {
+        rawImage.uvRect = new Rect(rawImage.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, rawImage.uvRect.size);
+    }
+}
